c1c37a72b753144f62b44ae1a4d072f52adc3c59
alok.singh <alsingh@deloitte.com>
2018-01-29 16:26:41 +0530

    Register Module changes

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  matcher = new MyErrorStateMatcher();
   registerForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder
@@ -19,8 +27,16 @@ export class RegisterComponent implements OnInit {
 
   createForm(){
     this.registerForm = this.formBuilder.group({
-      employeeName : ['', Validators.required],
-      employeeNumber : ['', Validators.required]
+      employeeName : ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(13),
+        Validators.maxLength(30),
+      ])],
+      employeeNumber : ['', Validators.required],
+      employeePassword : ['', Validators.required],
+      employeeDesignation : ['', Validators.required],
+      employeeServiceLine : ['', Validators.required],
+      employeeRole : ['', Validators.required]
     });
   }
 
