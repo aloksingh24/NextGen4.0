0e60f5377c143d76e3904ee75f11dd908ed25f84
alok.singh <alsingh@deloitte.com>
2018-01-31 08:38:51 +0530

    Service class chanegs

---
+++ frontend/src/app/services.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class ServicesService {
+  domain: "http://localhost:8080";
+  constructor(
+    private http: Http
+  ) { }
+
+  registereEmployee(employee){
+    return this.http.post('http://localhost:8080/authentication/register',employee).map(res => res.json());
+  }
+
+}
